16fd34e7fe6063b96a7d40951605f433dc7ab2e9
teste <teste@MacBook-Pro-de-Heitor.local>
2023-07-09 19:45:06 -0300

    turbo repo

---
+++ packages/docs/storybook-static/assets/Home.stories-dbc184c5.js
@@ -0,0 +1,39 @@
+import{M as r}from"./chunk-PCJTTTQV-f45ab028.js";import{j as e}from"./jsx-runtime-94f6e698.js";import{u as a}from"./index-1d576ef5.js";import"./iframe-01cf0e6a.js";import"../sb-preview/runtime.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-02b7423d.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-356e4a49.js";function o(n){const s=Object.assign({h1:"h1",p:"p",h3:"h3",ul:"ul",li:"li",code:"code"},a(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Home"}),`
+`,e.jsx(s.h1,{id:"design-system",children:"Design System"}),`
+`,e.jsx(s.p,{children:`Construa e escale seu Design System com facilidade.
+Cuidando de todos os aspectos, desde uma documentação
+poderosa até uma gestão eficiente do sistema de design,
+esse ecossistema poderoso acompanha o crescimento do seu design system.
+Todos os recursos essenciais para estabelecer o sistema de design estão
+disponíveis gratuitamente, e para sempre. Otimize sua experiência agora
+mesmo!`}),`
+`,e.jsx(s.h3,{id:"para-os-times",children:"PARA OS TIMES"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Consistência e coerência visual;"}),`
+`,e.jsx(s.li,{children:"Menos decisões de design;"}),`
+`,e.jsx(s.li,{children:"Maior direção criativa;"}),`
+`,e.jsx(s.li,{children:"Fluxo de trabalho eficiente;"}),`
+`,e.jsx(s.li,{children:"Melhor comunicação entre designers e desenvolvedores."}),`
+`]}),`
+`,e.jsx(s.h3,{id:"para-os-usuários",children:"PARA OS USUÁRIOS"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Experiência de uso unificada;"}),`
+`,e.jsx(s.li,{children:"Saber quais funcionalidades esperar dos componentes;"}),`
+`,e.jsx(s.li,{children:"Saber onde encontrar determinadas funcionalidades na página;"}),`
+`,e.jsx(s.li,{children:"Saber como usar uma funcionalidade para atingir seu objetivo;"}),`
+`,e.jsx(s.li,{children:"Não se esforçar para entender o significado de elementos desconhecidos;"}),`
+`,e.jsx(s.li,{children:"Aumentar a sensação de domínio das ferramentas e a habilidade de concluir suas atividades melhorando sua satisfação com a experiência."}),`
+`]}),`
+`,e.jsx(s.h3,{id:"para-o-negócio",children:"PARA O NEGÓCIO"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Melhores estimativas para desenvolvimento;"}),`
+`,e.jsx(s.li,{children:"Menos horas escrevendo códigos e fazendo ajustes de handoff;"}),`
+`,e.jsx(s.li,{children:"Protótipos de alta fidelidade finalizados em menor tempo;"}),`
+`,e.jsx(s.li,{children:"Designers dedicando mais tempo validando as soluções com usuários;"}),`
+`,e.jsx(s.li,{children:"Produtos lançados mais rápido e com mais qualidade;"}),`
+`,e.jsx(s.li,{children:"Times mais produtivos;"}),`
+`,e.jsx(s.li,{children:"Usuários mais satisfeitos;"}),`
+`]}),`
+`,e.jsx(s.h1,{id:"design-token",children:"Design Token"}),`
+`,e.jsxs(s.p,{children:["Os ",e.jsx(s.code,{children:"tokens"})," de design são uma fonte única de verdade para nomear e armazenar decisões de design"]})]})}function d(n={}){const{wrapper:s}=Object.assign({},a(),n.components);return s?e.jsx(s,{...n,children:e.jsx(o,{...n})}):o(n)}const t=()=>{throw new Error("Docs-only story")};t.parameters={docsOnly:!0};const i={title:"Home",tags:["stories-mdx"],includeStories:["__page"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:d};const M=["__page"];export{M as __namedExportsOrder,t as __page,i as default};
+//# sourceMappingURL=Home.stories-dbc184c5.js.map
