0120c1d6a52d781943c0aa2da10e5da03c6433a4
design-scale <138529597+design-scale@users.noreply.github.com>
2023-07-10 20:40:41 -0300

    Delete packages/docs/storybook-static directory

--- packages/docs/storybook-static/assets/preview-96c7b90e.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-01cf0e6a.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-6f26bc73.js"),["./DocsRenderer-EYKKDMVH-6f26bc73.js","./chunk-PCJTTTQV-f45ab028.js","./iframe-01cf0e6a.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./index-02b7423d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-96c7b90e.js.map
