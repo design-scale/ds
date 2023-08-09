304fc12342a1c623e22535dd221ac12445ea5b98
Modular Design <modulardesign@Mac-Mini.local>
2023-08-09 17:13:56 -0300

    MultiStep

--- packages/react/src/components/MultiStep/index.tsx
+++ packages/react/src/components/MultiStep/index.tsx
@@ -20,3 +20,9 @@ export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
     </MultiStepContainer>
   )
 }
+
+
+//let length = 8
+//const result = Array.from({length}, (_, i) => i + 1)
+// O segundo argumento da função Array.from() é uma função de mapeamento que é chamada para cada item no array resultante. Neste caso, o argumento _ é ignorado (é comum usar _ para argumentos não utilizados) e i é o índice do item. i + 1 é usado para gerar uma sequência de números começando de 1, já que os índices começam de 0.
+//console.log(result)
