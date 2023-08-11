b6c3bd501c832f9faf00541d6c3ed6d1442fa185
Modular Design <modulardesign@Mac-Mini.local>
2023-08-11 11:35:22 -0300

    change only preview.js to tsx

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,26 +1,30 @@
-module.exports = {
-  "stories": [
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    
     "../src/pages/**/*.stories.mdx",
     "../src/stories/**/*.stories.tsx"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
+    '@storybook/addon-a11y'
+    // 'storybook-dark-mode'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  "features": {
-    "storyStoreV7": true
+  docs: {
+    autodocs: "tag",
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
-    }
 
+  viteFinal: (config, {configType})=> {
+    if(configType === 'PRODUCTION'){
+      config.base = '/ds'
+    }
     return config
   }
-}
+};
+export default config;
